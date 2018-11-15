2be8c52bceadb8e3d5645ac2a7d5ad829d85a987
lmcgrana <lmcgrana@iu.edu>
2018-11-15 11:03:50 -0500

    Updates banner file

--- gulpfile.js
+++ gulpfile.js
@@ -16,26 +16,35 @@ const cssnano = require('gulp-cssnano');
 const autoprefixer = require('autoprefixer')
 const package = require('./package.json');
 
-// Get the current year for copyright in the banner
-const currentYear = new Date().getFullYear();
+// Create the string for the verion number banner.
+const banner = `/*! ${package.name} - @version ${package.version}
 
-const banner = `/*!
- * ${package.name} - @version ${package.version}
- *
- * Copyright (c) ${currentYear}, The Trustees of Indiana University
+* Copyright (c) 2018 TheTrustees of Indiana University
 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+* Licensed under the BSD 3-Clause License.
 
- * http://www.apache.org/licenses/LICENSE-2.0
-
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*   1.Redistributions of source code must retain the above copyright notice,
+*   this list of conditions and the following disclaimer.
+*   2.Redistributions in binary form must reproduce the above copyright notice,
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
+*   3.Neither the name of the copyright holder nor the names of its
+*   contributors may be used to endorse or promote products derived from
+*   this software without specific prior written permission.
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*/
 
 `;
 
@@ -182,6 +191,10 @@ gulp.task('css:prefix', function () {
     .pipe(gulp.dest('dist/css/'));
 });
 
+/**
+ * NOTE: We don't need to automate the banner on this project because is is already
+ * at the top of the Sass source entry file.
+ *
 gulp.task('css:header', function () {
   gulp.src('dist/css/' + package.name + '.css')
     .pipe(header(banner, { package: package }))
@@ -191,6 +204,7 @@ gulp.task('css:header', function () {
     .pipe(header(banner, { package: package }))
     .pipe(gulp.dest('dist/css/'));
 });
+ */
 
 // Compiles, prefixes, minifies, and versions CSS
 gulp.task('css:release', function(done) {
