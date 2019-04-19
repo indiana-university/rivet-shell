2e7d2a249d30a1bdb9b80b9710e64c04c146dafb
lmcgrana <lmcgrana@iu.edu>
2019-04-19 15:23:17 -0400

    Removed unused shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,4 @@
-module.exports = function (eleventyConfig) {
-  // Universal Shortcodes (Adds to Liquid, Nunjucks, Handlebars)
-  eleventyConfig.addPairedShortcode('alert', function (alertContent, variant, title) {
-    return `<div class="rvt-alert rvt-alert--${variant} rvt-m-tb-md">
-        <div class="rvt-alert__title" id="warning-alert-title">${title}</div>
-        <p class="rvt-alert__message">${alertContent}</p>
-      </div>`;
-  });
-  
+module.exports = function (eleventyConfig) {  
   eleventyConfig.addPassthroughCopy('src/img');
   
   return {
